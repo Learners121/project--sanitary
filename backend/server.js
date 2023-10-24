7f62fd844b495951749742130b377564ecc35125
shanwaz-78 <mohammed.shanwaz78@gmail.com>
2023-10-24 17:14:23 +0530

    Added add-product Functionality and refector some UI Components

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,19 @@
-import 'dotenv/config';
-import express from 'express';
-import cors from 'cors';
-import routes from './routes/index.js';
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import routes from "./routes/index.js";
 
 const app = express();
 
-app.use(cors({ origin: '*' }));
+app.use(cors({ origin: "*" }));
 
-app.use('/user', routes.userRoute);
-app.use('/add-product', routes.addProductRoute); // shahnavaz
-app.use('/view-product', routes.viewProductRoute); // juber
-app.use('/billing-history', routes.billingHistoryRoute); // mofique
-app.use('/pending-bills', routes.pendingBillsRoute); // uzair
+app.use(bodyParser.json())
+app.use("/user", routes.userRoute);
+app.use("/add-product", routes.addProductRoute); // shahnavaz
+app.use("/view-product", routes.viewProductRoute); // juber
+app.use("/billing-history", routes.billingHistoryRoute); // mofique
+app.use("/pending-bills", routes.pendingBillsRoute); // uzair
 
 app.listen(process.env.PORT, () => {
   console.log(`http://localhost:${process.env.PORT}`);
