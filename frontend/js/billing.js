0ec7c194416c2359ac61e4394f93911864c50743
shanwaz-78 <mohammed.shanwaz78@gmail.com>
2023-11-10 15:35:56 +0530

    Initial Commit

--- frontend/js/billing.js
+++ frontend/js/billing.js
@@ -9,7 +9,6 @@ function randerProductList(productList) {
     <td>${item.productName}</td>
     <td>${item.address}</td>
     <td>${item.number}</td>
-    <td><input type="button" value="Edit" id="edit" class="edit-btn" /></td>
     <td>
       <input
         type="button"
